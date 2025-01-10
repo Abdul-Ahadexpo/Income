d074da53e9a36fa911263685bdaf4a1023cefe77
Abdul Ahad <adahadd4845e@gmail.com>
2025-01-10 13:14:02 +0600

    aa

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDSWMPmzwyRoJf6ymBA56sqEu0hZRPXAFA",
+  authDomain: "dorochat-830bb.firebaseapp.com",
+  databaseURL: "https://dorochat-830bb-default-rtdb.firebaseio.com",
+  projectId: "dorochat-830bb",
+  storageBucket: "dorochat-830bb.firebasestorage.app",
+  messagingSenderId: "390410096076",
+  appId: "1:390410096076:web:ef0cb4978c93b6be96a169",
+};
